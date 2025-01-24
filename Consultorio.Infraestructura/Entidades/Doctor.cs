19a7c15519816509ba40c66dea56a77d03cebc17
Ivan Herrera <ivan.herrera@techsoft.com.mx>
2025-01-24 15:04:09 -0700

    Crear entidad Doctor

---
+++ Consultorio.Infraestructura/Entidades/Doctor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultorio.Infraestructura.Entidades
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+
+        public Doctor(int id, string nombre, string direccion, string telefono, string email)
+        {
+            Id = id;
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+            Email = email;
+
+            Validar(this);
+        }
+
+        private void Validar(Doctor doctor)
+        {
+            if (string.IsNullOrEmpty(doctor.Nombre))
+                throw new InvalidOperationException("El nombre del proveedor es requerido");
+
+            if (string.IsNullOrEmpty(doctor.Direccion))
+                throw new InvalidOperationException("La dirección del proveedor es requerida");
+
+            if (string.IsNullOrEmpty(Email))
+                throw new InvalidOperationException("El email del proveedor es requerido");
+
+            if(doctor.Id <= 0)
+                throw new InvalidOperationException("El id del proveedor es requerido");
+
+        }
+    }
+}
