2688970f8870863ed0c6d700f0a1a8c08b98c08e
Ivan Herrera <ivan.herrera@techsoft.com.mx>
2025-01-24 15:11:51 -0700

    Agregar formulario Doctores

---
+++ MyAplicacion/frmDoctores.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MyAplicacion
+{
+    partial class frmDoctores
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnGuardar = new Button();
+            txtId = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtNombre = new TextBox();
+            label3 = new Label();
+            txtDireccion = new TextBox();
+            label4 = new Label();
+            txtTelefono = new TextBox();
+            label5 = new Label();
+            txtEmail = new TextBox();
+            SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(69, 359);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(94, 29);
+            btnGuardar.TabIndex = 0;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(69, 60);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(168, 27);
+            txtId.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(69, 37);
+            label1.Name = "label1";
+            label1.Size = new Size(22, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Id";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(69, 96);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(69, 119);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(168, 27);
+            txtNombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(69, 159);
+            label3.Name = "label3";
+            label3.Size = new Size(72, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Dirección";
+            // 
+            // txtDireccion
+            // 
+            txtDireccion.Location = new Point(69, 182);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(168, 27);
+            txtDireccion.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(69, 224);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Telefono";
+            // 
+            // txtTelefono
+            // 
+            txtTelefono.Location = new Point(69, 247);
+            txtTelefono.Name = "txtTelefono";
+            txtTelefono.Size = new Size(168, 27);
+            txtTelefono.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(69, 288);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 20);
+            label5.TabIndex = 10;
+            label5.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(69, 311);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(168, 27);
+            txtEmail.TabIndex = 9;
+            // 
+            // frmDoctores
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(txtEmail);
+            Controls.Add(label4);
+            Controls.Add(txtTelefono);
+            Controls.Add(label3);
+            Controls.Add(txtDireccion);
+            Controls.Add(label2);
+            Controls.Add(txtNombre);
+            Controls.Add(label1);
+            Controls.Add(txtId);
+            Controls.Add(btnGuardar);
+            Name = "frmDoctores";
+            Text = "Doctores";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnGuardar;
+        private TextBox txtId;
+        private Label label1;
+        private Label label2;
+        private TextBox txtNombre;
+        private Label label3;
+        private TextBox txtDireccion;
+        private Label label4;
+        private TextBox txtTelefono;
+        private Label label5;
+        private TextBox txtEmail;
+    }
+}
